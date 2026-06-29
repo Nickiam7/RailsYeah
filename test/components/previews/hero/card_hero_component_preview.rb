24f0a157c1cfa2c61f5a4d82923fd5d599bfcf5a
nmcneany <nmcneany@gmail.com>
2026-06-28 20:58:21 -0500

    Revert "Add alternate /v2 landing page (Udemy-style course-card hero)"
    This reverts commit 5975876a84fc358e4b83de83e02d0fc64a5e3b36.

--- test/components/previews/hero/card_hero_component_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Preview at /rails/view_components/hero/card_hero_component
-class Hero::CardHeroComponentPreview < ViewComponent::Preview
-  def default
-    render(Hero::CardHeroComponent.new)
-  end
-end
